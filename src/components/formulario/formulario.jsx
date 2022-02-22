85352bc8b61e46ce004c626b22b501464b83e2bf
teodc888 <teodc4447@gmail.com>
2022-02-21 21:29:10 -0300

    casi listo

--- src/components/formulario/formulario.jsx
+++ src/components/formulario/formulario.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import FormularioAuto from "../autos/formularioAuto/formularioAuto";
 import FormularioMoto from "../motos/formularioMoto/formularioMoto";
+import FormularioRepuesto from "../repuestos/formularioRepuesto/formularioRepuesto";
 
 import { Button, Stack, Typography } from "@mui/material";
 export default function Formulario() {
@@ -17,6 +18,11 @@ export default function Formulario() {
     setTipo("moto");
     };
 
+    const handleClickRepuesto = (e) => {
+      e.preventDefault();
+      setTipo("repuesto");
+      };
+
   console.log(tipo);
   return (
     <>
@@ -24,21 +30,25 @@ export default function Formulario() {
         <FormularioAuto />
       ) : tipo === "moto" ? (
         <FormularioMoto setTipo={setTipo}/>
+      ) : tipo === "repuesto" ? (
+        <FormularioRepuesto setTipo={setTipo}/>
+        
       ) : (
         <Stack
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          spacing={2}
-        >
-          <Typography variant="h2" component="div">
-            Formulario
-          </Typography>
-          <Button onClick={handleClickAuto}>Autos</Button>
-          <Button onClick={handleClickMoto}>Motos</Button>
-          <Button>Repuestos</Button>
-        </Stack>
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        spacing={2}
+      >
+        <Typography variant="h2" component="div">
+          Formulario
+        </Typography>
+        <Button onClick={handleClickAuto}>Autos</Button>
+        <Button onClick={handleClickMoto}>Motos</Button>
+        <Button onClick={handleClickRepuesto}>Repuestos</Button>
+      </Stack>
       )}
+      
     </>
   );
 }
