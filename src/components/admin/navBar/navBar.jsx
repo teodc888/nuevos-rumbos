5d08de9bc7e439e3c7f8fc7dec00b77107ddcddf
mateo <teodc4447@gmail.com>
2022-03-01 15:14:16 -0300

    stilos landing

--- src/components/admin/navBar/navBar.jsx
+++ src/components/admin/navBar/navBar.jsx
@@ -1,37 +1,45 @@
-import React from 'react';
+import React from "react";
 
 //Mui
-import { AppBar, Box, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 
 //Redux
-import { useDispatch } from 'react-redux';
-import { Login } from '../../../redux/actions/index';
+import { useDispatch } from "react-redux";
+import { Login } from "../../../redux/actions/index";
 
 //Router
-import { useNavigate } from 'react-router';
+import { useNavigate } from "react-router";
 
 //Swal
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
 export default function NavBarAdmin() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const handleNavigateHome = () => {
-        navigate("/");
-    };
+  const handleNavigateHome = () => {
+    navigate("/");
+  };
 
-    const handleClickLogout = () => {
-        dispatch(Login(""));
-        Swal.fire({
-            text: 'se cerro sesion',
-            confirmButtonText: 'Ok',
-            icon: 'success',
-            width: 'auto',
-            timer: 2500,
-        });
-    };
+  const handleClickLogout = () => {
+    dispatch(Login(""));
+    Swal.fire({
+      text: "se cerro sesion",
+      confirmButtonText: "Ok",
+      icon: "success",
+      width: "auto",
+      timer: 2500,
+    });
+    navigate("/");
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -50,7 +58,9 @@ export default function NavBarAdmin() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             ADMIN
           </Typography>
-          <Button color="inherit" onClick={handleClickLogout}>Cerrar Sesion</Button>
+          <Button color="inherit" onClick={handleClickLogout}>
+            Cerrar Sesion
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,21 +1,30 @@
-import React, {useEffect} from "react";
-import { Button, Grid, Card, CardMedia, Typography } from "@mui/material";
+import React, { useEffect } from "react";
+
+//Mui
+import { Button, Grid, Card, CardMedia, Typography, Box } from "@mui/material";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import TwoWheelerIcon from "@mui/icons-material/TwoWheeler";
+import BuildIcon from "@mui/icons-material/Build";
+
+//Type
 import Typed from "react-typed";
-import {useNavigate} from 'react-router';
+
+//Router
+import { useNavigate } from "react-router";
 
 export default function Landing() {
   const navigate = useNavigate();
 
   const navigateToAuto = () => {
-    navigate('/autos');
+    navigate("/autos");
   };
 
   const navigateToMoto = () => {
-    navigate('/motos');
+    navigate("/motos");
   };
 
   const navigateToRepuestos = () => {
-    navigate('/repuestos');
+    navigate("/repuestos");
   };
 
   useEffect(() => {
@@ -24,81 +33,132 @@ export default function Landing() {
 
   return (
     <>
-      <Grid
-        container
-        spacing={{ xs: 2, md: 2, sm: 2 }}
-        columns={{ xs: 16, sm: 16, md: 16 }}
-      >
+      <Box sx={{ width: "100%", marginTop: "1%" }}>
         <Grid
-          className="App"
-          item
-          xs={16}
-          sm={8}
-          md={8}
-          sx={{ margin: "auto" }}
+          container
+          spacing={{ xs: 2, md: 2, sm: 2 }}
+          columns={{ xs: 16, sm: 16, md: 16 }}
         >
-          <Typography
-            variant="h3"
-            component="div"
-            sx={{marginBottom: "3%"}}
-          >
-            NUEVOS RUMBOS
-          </Typography>
-          <Typography
-            variant="h5"
-            component="div"
-            sx={{ marginBottom: "5%" }}
-          >
-            La mejor pagina de importaciones de -
-            <Typed
-              strings={[" AUTOS", "  MOTOS", "  REPUESTOS"]}
-              typeSpeed={65}
-              backSpeed={65}
-              loop
-            />
-          </Typography>
-
           <Grid
-            container
-            spacing={{ xs: 2, md: 2, sm: 2 }}
-            columns={{ xs: 6, sm: 6, md: 6 }}
-            sx={{ marginTop: "1%" }}
+            className="App"
+            item
+            xs={16}
+            sm={8}
+            md={8}
+            sx={{ margin: "auto" }}
           >
-            <Grid className="App" item xs={6} sm={2} md={2}>
-              <Button variant="contained" sx={{ bgcolor: "#4a148c", color:"white" }} onClick={navigateToAuto}>
-                Autos
-              </Button>
-            </Grid>
-            <Grid item xs={6} sm={2} md={2}>
-              <Button variant="contained" sx={{ bgcolor: "#4a148c", color:"white" }} onClick={navigateToMoto}>
-                Motos
-              </Button>
-            </Grid>
-            <Grid item xs={6} sm={2} md={2}>
-              <Button variant="contained" sx={{ bgcolor: "#4a148c", color:"white" }} onClick={navigateToRepuestos}>
-                Repuestos
-              </Button>
+            <Typography
+              variant="h2"
+              component="div"
+              sx={{ marginBottom: "3%", display: { xs: "none", md: "block" } }}
+              fontFamily="Segoe UI Symbol"
+            >
+              NUEVOS RUMBOS
+            </Typography>
+            <Typography
+              variant="h4"
+              component="div"
+              sx={{
+                marginBottom: "5%",
+                display: { xs: "block", md: "none", sm: "none" },
+                marginTop: "3%",
+              }}
+              fontFamily="Segoe UI Symbol"
+            >
+              NUEVOS RUMBOS
+            </Typography>
+            <Typography
+              variant="h4"
+              component="div"
+              sx={{
+                marginBottom: "3%",
+                display: { xs: "none", md: "none", sm: "block" },
+              }}
+              fontFamily="Segoe UI Symbol"
+            >
+              NUEVOS RUMBOS
+            </Typography>
+            <Typography
+              variant="h5"
+              component="div"
+              sx={{ marginBottom: "5%", display: { xs: "none", md: "block" } }}
+            >
+              La mejor pagina de importaciones de -
+              <Typed
+                strings={[" AUTOS", "  MOTOS", "  REPUESTOS"]}
+                typeSpeed={65}
+                backSpeed={65}
+                loop
+              />
+            </Typography>
+            <Typography
+              variant="h9"
+              component="div"
+              sx={{ marginBottom: "5%", display: { xs: "block", md: "none" } }}
+            >
+              La mejor pagina de importaciones de -
+              <Typed
+                strings={[" AUTOS", "  MOTOS", "  REPUESTOS"]}
+                typeSpeed={65}
+                backSpeed={65}
+                loop
+              />
+            </Typography>
+
+            <Grid
+              container
+              spacing={{ xs: 3, md: 2, sm: 2 }}
+              columns={{ xs: 6, sm: 6, md: 6 }}
+              sx={{ marginTop: "1%" }}
+            >
+              <Grid className="App" item xs={6} sm={6} md={2}>
+                <Button
+                  variant="contained"
+                  sx={{ bgcolor: "#4a148c", color: "white" }}
+                  onClick={navigateToAuto}
+                >
+                  <DirectionsCarIcon sx={{ marginRight: "10%" }} /> Autos
+                </Button>
+              </Grid>
+              <Grid item xs={6} sm={6} md={2}>
+                <Button
+                  variant="contained"
+                  sx={{ bgcolor: "#4a148c", color: "white" }}
+                  onClick={navigateToMoto}
+                >
+                  <TwoWheelerIcon sx={{ marginRight: "10%" }} /> Motos
+                </Button>
+              </Grid>
+              <Grid item xs={6} sm={6} md={2}>
+                <Button
+                  variant="contained"
+                  sx={{ bgcolor: "#4a148c", color: "white" }}
+                  onClick={navigateToRepuestos}
+                >
+                  <BuildIcon sx={{ marginRight: "10%" }} /> Repuestos
+                </Button>
+              </Grid>
             </Grid>
           </Grid>
+          <Grid
+            className="App"
+            item
+            xs={16}
+            sm={8}
+            md={8}
+            sx={{ marginTop: "6%" }}
+          >
+            <Card sx={{ maxWidth: 800, margin: "auto" }}>
+              <CardMedia
+                component="img"
+                height="500"
+                image="https://scontent.fcor2-2.fna.fbcdn.net/v/t1.6435-9/121409666_3270786793044323_821596622646100409_n.png?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=LVAoisEStmcAX8zADv0&_nc_ht=scontent.fcor2-2.fna&oh=00_AT8MGj0dVLhO4mNBvUO-eRLpWsk6cyKvV3ONqkCQ_R0WkA&oe=62342CCE"
+                alt="green iguana"
+              />
+            </Card>
+          </Grid>
         </Grid>
-        <Grid
-          className="App"
-          item
-          xs={16}
-          sm={8}
-          md={8}
-          sx={{ marginTop: "6%" }}
-        >
-          <Card sx={{ maxWidth: 800 }}>
-            <CardMedia
-              component="img"
-              height="500"
-              image="https://scontent.fcor2-2.fna.fbcdn.net/v/t1.6435-9/121409666_3270786793044323_821596622646100409_n.png?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=LVAoisEStmcAX8zADv0&_nc_ht=scontent.fcor2-2.fna&oh=00_AT8MGj0dVLhO4mNBvUO-eRLpWsk6cyKvV3ONqkCQ_R0WkA&oe=62342CCE"
-              alt="green iguana"
-            />
-          </Card>
-        </Grid>
-      </Grid>
+      </Box>
     </>
   );
 }
