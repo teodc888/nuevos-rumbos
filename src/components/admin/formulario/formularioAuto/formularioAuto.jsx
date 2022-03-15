9684ef44f9f2f7ab8134a31d2974bd11cbcdbe05
santiago clemenzi <clemenzisantiago@gmail.com>
2022-03-14 21:19:20 -0300

    aplicacion del btn info

--- src/components/admin/formulario/formularioAuto/formularioAuto.jsx
+++ src/components/admin/formulario/formularioAuto/formularioAuto.jsx
@@ -180,6 +180,8 @@ export default function FormularioAuto() {
 							name="kilometros"
 							onChange={handleChange}
 							type="number"
+							inputProps={{ min: 0 }}
+							min={0}
 							value={input.kilometros}
 							required
 							fullWidth
@@ -191,13 +193,15 @@ export default function FormularioAuto() {
 							name="puertas"
 							onChange={handleChange}
 							type="number"
+							inputProps={{ min: 0 }}
+							min={0}
 							value={input.puertas}
 							required
 							fullWidth
 							sx={{ marginTop: '2%' }}
 						/>
 						<TextField
-              select
+							select
 							label="combustible"
 							name="combustible"
 							value={input.combustible}
@@ -231,6 +235,8 @@ export default function FormularioAuto() {
 							name="precio"
 							onChange={handleChange}
 							type="number"
+							inputProps={{ min: 0 }}
+							min={0}
 							value={input.precio}
 							required
 							fullWidth
@@ -274,45 +280,109 @@ export default function FormularioAuto() {
 							name="año"
 							onChange={handleChange}
 							type="number"
+							inputProps={{ min: 0 }}
+							min={0}
 							value={input.año}
 							required
 							fullWidth
 							sx={{ marginTop: '2%' }}
 						/>
+						<Grid item xs={12}>
+							<Grid item xs={12}>
+								<TextField
+									select
+									label="carroceria"
+									name="carroceria"
+									value={input.carroceria}
+									onChange={handleSelectChangeCarroceria}
+									required
+									fullWidth
+									sx={{ marginTop: '2%' }}
+								>
+									<MenuItem name="carroceria" value={'sedan'}>
+										Sedán
+									</MenuItem>
+									<MenuItem name="carroceria" value={'compacto'}>
+										Compacto
+									</MenuItem>
+									<MenuItem name="carroceria" value={'familiar'}>
+										Familiar
+									</MenuItem>
+									<MenuItem name="carroceria" value={'Coupe'}>
+										Coupé
+									</MenuItem>
+									<MenuItem name="carroceria" value={'todoterreno'}>
+										Todoterreno
+									</MenuItem>
+									<MenuItem name="carroceria" value={'descapotable'}>
+										Descapotable
+									</MenuItem>
+									<MenuItem name="carroceria" value={'suv'}>
+										SUV
+									</MenuItem>
+								</TextField>
+							</Grid>
+							<Grid item xs={12}>
+								<IconButton
+									onMouseEnter={handlePopoverOpen}
+									onMouseLeave={handlePopoverClose}
+								>
+									<InfoIcon />
+								</IconButton>
+								<Popover
+									id="mouse-over-popover"
+									sx={{
+										pointerEvents: 'none',
+									}}
+									open={open}
+									anchorEl={anchorEl}
+									anchorOrigin={{
+										vertical: 'bottom',
+										horizontal: 'left',
+									}}
+									transformOrigin={{
+										vertical: 'top',
+										horizontal: 'left',
+									}}
+									onClose={handlePopoverClose}
+									disableRestoreFocus
+								>
+									<Typography sx={{ p: 1 }}>
+										Sedán o berlina: carrocería de tres volúmenes.
+									</Typography>
+									<Typography sx={{ p: 1 }}>
+										Compacto: carrocería de tres o cinco puertas en la que el
+										portón trasero incluye la luneta, haciendo posible el acceso
+										al habitáculo de los pasajeros.
+									</Typography>
+									<Typography sx={{ p: 1 }}>
+										Familiar: Carrocería de techo elevado hasta el portón
+										trasero que permite un amplio espacio de acceso y carga.
+									</Typography>
+									<Typography sx={{ p: 1 }}>
+										Coupé: carrocería de dos o tres volúmenes con dos puertas
+										acristaladas.
+									</Typography>
+									<Typography sx={{ p: 1 }}>
+										Todoterreno: diseñado para ofrecer sus mejores prestaciones
+										fuera de las carreteras asfaltadas y dotado de una mayor
+										altura al suelo, así como interior en el habitáculo.
+									</Typography>
+									<Typography sx={{ p: 1 }}>
+										Descapotable: techo plegable o capota, generalmente de tela
+										o rígida articulada.
+									</Typography>
+									<Typography sx={{ p: 1 }}>
+										SUV: la carrocería de moda actualmente, parte de la base de
+										un turismo, pero gana altura con respecto al suelo e
+										interior, aunque no permite un uso en campo equiparable al
+										de los todoterreno.
+									</Typography>
+								</Popover>
+							</Grid>
+						</Grid>
 						<TextField
-              select
-							label="carroceria"
-							name="carroceria"
-							value={input.carroceria}
-							onChange={handleSelectChangeCarroceria}
-							required
-							fullWidth
-							sx={{ marginTop: '2%' }}
-						>
-							<MenuItem name="carroceria" value={'sedan'}>
-								Sedán
-							</MenuItem>
-							<MenuItem name="carroceria" value={'compacto'}>
-								Compacto
-							</MenuItem>
-							<MenuItem name="carroceria" value={'familiar'}>
-								Familiar
-							</MenuItem>
-							<MenuItem name="carroceria" value={'Coupe'}>
-								Coupé
-							</MenuItem>
-							<MenuItem name="carroceria" value={'todoterreno'}>
-								Todoterreno
-							</MenuItem>
-							<MenuItem name="carroceria" value={'descapotable'}>
-								Descapotable
-							</MenuItem>
-							<MenuItem name="carroceria" value={'suv'}>
-								SUV
-							</MenuItem>
-						</TextField>
-						<TextField
-              select
+							select
 							label="GNV"
 							name="gnv"
 							value={input.gnv}
@@ -328,9 +398,7 @@ export default function FormularioAuto() {
 								No
 							</MenuItem>
 						</TextField>
-						<InputLabel id="demo-simple-select-label" sx={{ marginTop: '2%' }}>
-							Descripcion
-						</InputLabel>
+						<InputLabel sx={{ marginTop: '2%' }}>Descripcion</InputLabel>
 						<TextareaAutosize
 							onChange={handleChange}
 							name="descripcion"
@@ -338,72 +406,9 @@ export default function FormularioAuto() {
 							maxRows={10}
 							style={{ width: '90%', maxHeight: 150 }}
 						/>
-						<InputLabel id="demo-simple-select-label" sx={{ marginTop: '2%' }}>
-							Imagen
-						</InputLabel>
+						<InputLabel sx={{ marginTop: '2%' }}>Imagen</InputLabel>
 						<Input type="file" name="imagen" onChange={handleFiles} required />
 					</Grid>
-
-					{/* <Grid item xs={16}>
-						<Grid item xs={16}>
-							<IconButton
-								onMouseEnter={handlePopoverOpen}
-								onMouseLeave={handlePopoverClose}
-							>
-								<InfoIcon />
-							</IconButton>
-							<Popover
-								id="mouse-over-popover"
-								sx={{
-									pointerEvents: 'none',
-								}}
-								open={open}
-								anchorEl={anchorEl}
-								anchorOrigin={{
-									vertical: 'bottom',
-									horizontal: 'left',
-								}}
-								transformOrigin={{
-									vertical: 'top',
-									horizontal: 'left',
-								}}
-								onClose={handlePopoverClose}
-								disableRestoreFocus
-							>
-								<Typography sx={{ p: 1 }}>
-									Sedán o berlina: carrocería de tres volúmenes.
-								</Typography>
-								<Typography sx={{ p: 1 }}>
-									Compacto: carrocería de tres o cinco puertas en la que el
-									portón trasero incluye la luneta, haciendo posible el acceso
-									al habitáculo de los pasajeros.
-								</Typography>
-								<Typography sx={{ p: 1 }}>
-									Familiar: Carrocería de techo elevado hasta el portón trasero
-									que permite un amplio espacio de acceso y carga.
-								</Typography>
-								<Typography sx={{ p: 1 }}>
-									Coupé: carrocería de dos o tres volúmenes con dos puertas
-									acristaladas.
-								</Typography>
-								<Typography sx={{ p: 1 }}>
-									Todoterreno: diseñado para ofrecer sus mejores prestaciones
-									fuera de las carreteras asfaltadas y dotado de una mayor
-									altura al suelo, así como interior en el habitáculo.
-								</Typography>
-								<Typography sx={{ p: 1 }}>
-									Descapotable: techo plegable o capota, generalmente de tela o
-									rígida articulada.
-								</Typography>
-								<Typography sx={{ p: 1 }}>
-									SUV: la carrocería de moda actualmente, parte de la base de un
-									turismo, pero gana altura con respecto al suelo e interior,
-									aunque no permite un uso en campo equiparable al de los
-									todoterreno.
-								</Typography>
-							</Popover>
-						</Grid>
-					</Grid> */}
 				</Grid>
 				<Button
 					type="submit"
