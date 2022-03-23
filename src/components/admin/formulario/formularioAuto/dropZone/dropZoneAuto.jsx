25a58a43f6df7bea2c5413d07acefada93e56caa
santiago clemenzi <clemenzisantiago@gmail.com>
2022-03-23 20:23:56 -0300

    funcion onUpload lista

--- src/components/admin/formulario/formularioAuto/dropZone/dropZoneAuto.jsx
+++ src/components/admin/formulario/formularioAuto/dropZone/dropZoneAuto.jsx
@@ -3,27 +3,37 @@ import { useDropzone, FileRejection } from 'react-dropzone';
 // packages
 import { Grid } from '@mui/material';
 // components
-import {SingleFileUploadWithProgress} from './cargarArchivoIndividual';
+import { SingleFileUploadWithProgress } from './cargarArchivoIndividual';
 
-export interface UploadableFile{
-    file: File;
-    errors: FileError[];
+export interface UploadableFile {
+	file: File;
+	errors: FileError[];
 }
 
 const DropZone = () => {
-    // states 
-    const [files, setFiles] = useState([]);
+	// states
+	const [files, setFiles] = useState([]);
 
 	const onDrop = useCallback((accFiles: File[], RejFiles: FileRejection[]) => {
 		// Do something with the files
-        const mappedAcc = accFiles.map((file)=>({file, errors: []}));
-        setFiles((curr)=>[...curr, ...mappedAcc, ...RejFiles]);
+		const mappedAcc = accFiles.map((file) => ({ file, errors: [] }));
+		setFiles((curr) => [...curr, ...mappedAcc, ...RejFiles]);
 	}, []);
 	const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
 	// funcion eliminar imagen
-	const onDelete = (file:File) => {
-		setFiles((curr)=>curr.filter((fw) => fw.file !== file));
+	const onDelete = (file: File) => {
+		setFiles((curr) => curr.filter((fw) => fw.file !== file));
+	};
+	const onUpload = (file: File, url: string) => {
+		setFiles((curr) =>
+			curr.map((fw) => {
+				if (fw.file === file) {
+					return { ...fw, url };
+				}
+				return fw;
+			})
+		);
 	};
 
 	return (
@@ -32,8 +42,13 @@ const DropZone = () => {
 				<div {...getRootProps()}>
 					<input {...getInputProps()} />
 					<p>Drag 'n' drop some files here, or click to select files</p>
-					{files.map((fileWrapper, index) =>(
-						<SingleFileUploadWithProgress key={index} onDelete={onDelete} file={fileWrapper.file} />
+					{files.map((fileWrapper, index) => (
+						<SingleFileUploadWithProgress
+							key={index}
+							onDelete={onDelete}
+							onUpload={onUpload}
+							file={fileWrapper.file}
+						/>
 					))}
 				</div>
 			</Grid>
